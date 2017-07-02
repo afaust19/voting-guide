e1bed3e5ea7fe1f1c6f7093f57cea7f2bc19e8cf
AnnMarie Faust <amn5336@gmail.com>
2017-07-02 11:38:38 -0500

    Add User model class

--- src/main/java/com/faust/votingguide/controllers/UserController.java
+++ src/main/java/com/faust/votingguide/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.faust.votingguide.controllers;
 
 /**
- * Created by afaust on 7/1/17.
+ * Created by afaust on 7/2/17.
  */
 public class UserController {
 }
---
+++ src/main/java/com/faust/votingguide/models/User.java
@@ -0,0 +1,13 @@
+package com.faust.votingguide.models;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by afaust on 7/2/17.
+ */
+public class User {
+
+    @NotNull
+    private String username;
+
+}
