354c9f8c8752c872a6a195ce00af5ba678c5d961
AnnMarie Faust <amn5336@gmail.com>
2017-07-02 13:00:59 -0500

    Add UserController methods for registering new users

--- src/main/java/com/faust/votingguide/controllers/UserController.java
+++ src/main/java/com/faust/votingguide/controllers/UserController.java
@@ -1,8 +1,37 @@
 package com.faust.votingguide.controllers;
 
+import com.faust.votingguide.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
 /**
  * Created by afaust on 7/2/17.
  */
-
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "Register");
+        return "user/register";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String register(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute(user);
+            return "user/register";
+        }
+
+        model.addAttribute((user));
+        return "user/index";
+    }
 }
